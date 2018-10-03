77e375dfbde459c48ad1da374d8c56d8567f4dbb
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-10-04 00:29:02 +0500

    Bug Fixing

--- src/Screens/Fb_Post/Fb_Post.js
+++ src/Screens/Fb_Post/Fb_Post.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Typography from '@material-ui/core/Typography';
-import Toolbar from '@material-ui/core/Toolbar';
 import Navbar from '../../Components/Navbar/Navbar';
 import '../../App.css';
+
+
+
+
 
 class Fb_Post extends Component {
 constructor(){
@@ -13,35 +14,18 @@ constructor(){
   }
 }
 
-  // getImages(){
-  //   let counter = this.state ;
-  //   let post = this.props;
-  //   var a ;
-  //   for(counter=0 ;counter <= 8 ; counter++){      
-  //     a = counter;
-  //   }
-  //   console.log(a)
-  // }
 
   render() {
     const { post } = this.props;
-    // let counter = this.state ;
-    // var a ;
-    // let img = document.getElementById('img');
-    // for(counter=0 ;counter <= 8 ; counter++){      
-    //   a = counter;
-    //   console.log(img);
-    // }
-    // console.log(post.images[0]);
+
     return (
       <div className="App">
         <Navbar />
         <br />
-        <img src={post.images[0]} alt="logo" id="img" />
         {
         post.images.map((v , i)=>{
           console.log(v);
-          return <img src={v} alt="image"  />
+          return <img key={i} src={v} alt="image" className="render-image" />
         })
       }
       </div>
