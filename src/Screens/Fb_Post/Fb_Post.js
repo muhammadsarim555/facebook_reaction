477b4ce46b38f8067647416a5c17018e5fe45115
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-10-03 15:50:33 +0500

    Work has been started

---
+++ src/Screens/Fb_Post/Fb_Post.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../../App.css';
+
+class Fb_Post extends Component {
+  
+  render() {
+    console.log(this.props.post);
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default Fb_Post;
