42f96f4e2ad8cef45fae80d31cc5be5e4baa4ca4
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-10-04 20:16:41 +0500

    Added work

--- src/Screens/Fb_Post/Fb_Post.js
+++ src/Screens/Fb_Post/Fb_Post.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from '../../Components/Navbar/Navbar';
+import FacebookEmoji from 'react-facebook-emoji';
+import image from '../../Images/images.png'
 import '../../App.css';
 
 
@@ -7,37 +9,71 @@ import '../../App.css';
 
 
 class Fb_Post extends Component {
-constructor(){
-  super();
-  this.state = {
-    counter: 0,
+  constructor() {
+    super();
+    this.state = {
+      isLike: false,
+    }
   }
-}
 
+  emojis() {
+    return <div>
+      <FacebookEmoji type="like" size="sm" />
+      <FacebookEmoji type="love" size="sm" />
+      <FacebookEmoji type="yay" size="sm" />
+      <FacebookEmoji type="wow" size="sm" />
+      <FacebookEmoji type="angry" size="sm" />
+      <FacebookEmoji type="haha" size="sm" />
+      <FacebookEmoji type="sad" size="sm" />
+      {/* <FacebookEmoji type="like" size="sm"/> */}
+    </div>
+  }
+
+like(){
+    this.setState({
+      isLike: true,
+    })
+  }
+
+  dislike(){
+    this.setState({
+      isLike: false,
+    })
+  }
 
   render() {
     const { post } = this.props;
+    const {isLike} = this.state;
     console.log(post.likes.length)
     return (
       <div className="App">
+        {/* {this.emojis()} */}
         <Navbar />
-          <br/>
+        <br />
         <br />
         {
-        post.images.map((v , i)=>{
-          console.log(v);
-          return <div className="card-header" >
-          <img key={i} src={v} alt="image" className="render-image" />
-          <br/>
-            <span> {post.likes.length} </span>
-          <br/>
-            <button className="like-button"> Like </button>
-          
+          post.images.map((v, i) => {
+            console.log(v);
+            return <div className="card-header" >
+              <img key={i} src={v} alt="image" className="render-image" />
+              <br />
+              <span>
+                {/* <img src={image} className="logo" /> */}
+                {post.likes.length} </span>
+              <br />
+              {isLike && this.emojis() }
+              { !isLike ? 
+              <button className="like-button" onClick={this.like.bind(this)}> Like </button>
+                :
+              <button className="like-button" onClick={this.dislike.bind(this)}> Dislike </button>
+                
+
+            }
 
 
-</div>
-        })
-      }
+            </div>
+          })
+        }
       </div>
     );
   }
